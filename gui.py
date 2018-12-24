9b266b2071ba74e1bc2d1638e994a236f500c88a
Alan Davidson <alan@privacy.com>
2018-12-24 16:26:29 -0500

    Minor tweaks, add documentation, fix TODO (#3)
    * tweaks
    
    * add documentation, fix TODO

--- gui.py
+++ gui.py
@@ -8,21 +8,25 @@ from zoom_map import ZoomMap
 
 
 class _Context(tk.Text):
+    """
+    A display of surrounding code, with the relevant tokens highlighted. We will
+    have one of these for the token(s) represented by the column of the mouse
+    cursor, and another for its row.
+    """
     CONTEXT_COUNT = 3  # Lines to display before/after the current one
     # TODO: What about files with over 99,999 lines?
     LINE_NUMBER_WIDTH = 5  # Number of characters to allocate for line numbers
     PRELUDE_WIDTH = LINE_NUMBER_WIDTH + 2  # Line number, colon, space
-    # TODO: What about files with very long lines? They currently wrap around to
-    # the next line and push later context out of the widget. Should we truncate
-    # them instead? and if so, should we change which part gets cut based on the
-    # location of the token within the line?
+    # NOTE: Lines longer than TEXT_WIDTH get truncated, and any tokens off the
+    # end don't get shown/highlighted.
     TEXT_WIDTH = 80
 
     def __init__(self, tk_parent, data, zoom_map):
         height = 2 * self.CONTEXT_COUNT + 1
         width = self.PRELUDE_WIDTH + self.TEXT_WIDTH
         super().__init__(tk_parent, width=width, height=height,
-                         state=tk.DISABLED, font="TkFixedFont")
+                         state=tk.DISABLED, font="TkFixedFont", borderwidth=2,
+                         relief="ridge")
         self.pack()
         # TODO: Use a NamedTuple?
         self._tokens, self._lines, self._boundaries = data
@@ -46,7 +50,7 @@ class _Context(tk.Text):
         start = line_number - self.CONTEXT_COUNT - 1
         end   = line_number + self.CONTEXT_COUNT
         lines = ["{:>{}}: {}".format(i + 1, self.LINE_NUMBER_WIDTH,
-                                     self._lines[i])
+                                     self._lines[i][:80])
                  if 0 <= i < len(self._lines) else ""
                  for i in range(start, end)]
         text = "\n".join(lines)
@@ -57,8 +61,8 @@ class _Context(tk.Text):
         self.insert(tk.INSERT, text)
 
         # Highlight the tokens of interest...
-        (ar, ac) = self._boundaries[first_token_index][0]
-        (br, bc) = self._boundaries[last_token_index][1]
+        ar, ac = self._boundaries[first_token_index][0]
+        br, bc = self._boundaries[last_token_index][1]
         self.tag_add("token",
                      "{}.{}".format(self.CONTEXT_COUNT + 1,
                                     ac + self.PRELUDE_WIDTH),
@@ -96,11 +100,15 @@ class _Gui(tk.Frame):
 
 
 def launch(matrix, data_a, data_b):
+    """
+    Creates a new window for the GUI and runs the main program.
+    """
     root = tk.Tk()
 
     def _quit(event):
         root.destroy()
-    [root.bind("<Control-{}>".format(char), _quit) for char in "wWqQ"]
+    for char in "wWqQ":
+        root.bind("<Control-{}>".format(char), _quit)
 
     gui = _Gui(matrix, data_a, data_b, root)
     while True:
@@ -110,6 +118,8 @@ def launch(matrix, data_a, data_b):
         except UnicodeDecodeError:
             # Macs running Python 3.6 and older have a bug in TK where they
             # interpret scroll wheel events as though they should be UTF-8 even
-            # though they're not.
+            # though they're not. You can try upgrading TK, but Python 3.7 and
+            # later has its own built-in version of TK which doesn't have the
+            # bug.
             print("Macs with old versions of TK installed don't scroll "
                   "properly. Try upgrading Python to version 3.7 or later.")
