ce8a1bd437ffcc79aac0b6636981112626b7e0a8
Alan Davidson <alan@viam.com>
2024-12-14 10:36:27 -0500

    rename variables

--- find_duplicates.py
+++ find_duplicates.py
@@ -22,14 +22,14 @@ def get_lengths(matrix, is_single_file):
       diagonals of 5 pixels each whose endpoints are separated by a knight's
       move should have a value of 9.
     """
-    r, c = matrix.shape
-    distance_template = numpy.arange(r)[:, None] + numpy.arange(c)
+    nr, nc = matrix.shape
+    distance_template = numpy.arange(nr)[:, None] + numpy.arange(nc)
     # For each pixel, we need to keep track of the score it can achieve by
     # joining things further down and right from it, and the row and column of
     # the best such pixel to join it with.
-    scores = numpy.zeros((r, c), dtype=numpy.int32) - 1
-    next_r = numpy.zeros((r, c), dtype=numpy.int32) - 1
-    next_c = numpy.zeros((r, c), dtype=numpy.int32) - 1
+    scores = numpy.zeros((nr, nc), dtype=numpy.int32) - 1
+    next_r = numpy.zeros((nr, nc), dtype=numpy.int32) - 1
+    next_c = numpy.zeros((nr, nc), dtype=numpy.int32) - 1
 
     # Initialize the bottommost and rightmost edges to be the initial scores:
     # they cannot grow further down or right.
@@ -39,37 +39,38 @@ def get_lengths(matrix, is_single_file):
     # Then, walk the rest of matrix from bottom-right to top-left, with each
     # pixel growing as large as it can solely by joining things further right
     # and down from itself.
-    for i in range(r - 2, -1, -1):
-        for j in range(c - 2, -1, -1):
-            if matrix[i, j] == 0:  # Pixel is unset, so it should score -1.
+    for r in range(nr - 2, -1, -1):
+        for c in range(nc - 2, -1, -1):
+            if matrix[r, c] == 0:  # Pixel is unset, so it should score -1.
                 continue
             # Otherwise, it should be at least 1.
-            scores[i, j] = 1
+            scores[r, c] = 1
 
-            if is_single_file and i == j:
+            if is_single_file and r == c:
                 # A token compared to itself matches but shouldn't be
                 # considered an interesting group.
                 continue
 
-            candidates = scores[i+1:, j+1:]
-            distances = distance_template[:(r - i - 1), :(c - j - 1)]
+            candidates = scores[r+1:, c+1:]
+            distances = distance_template[:(nr - r - 1), :(nc - c - 1)]
             possible_scores = candidates - distances
             best_r, best_c = numpy.unravel_index(
                     numpy.argmax(possible_scores), candidates.shape)
             best_score = possible_scores[best_r, best_c]
             if best_score > 0:
-                scores[i, j] += best_score
-                next_r[i, j] = best_r + i + 1
-                next_c[i, j] = best_c + j + 1
+                scores[r, c] += best_score
+                next_r[r, c] = best_r + r + 1
+                next_c[r, c] = best_c + c + 1
 
     # Now, do the opposite: moving top-left to bottom-right, set the scores of
     # all pixels to be as large as we could find.
-    for i in range(r):
-        for j in range(c):
-            nr = next_r[i, j]
-            nc = next_c[i, j]
-            if nr < 0 or nc < 0:
+    # TODO: can this be numpy-icized? At least column-by-column, maybe?
+    for i in range(nr):
+        for j in range(nc):
+            r = next_r[i, j]
+            c = next_c[i, j]
+            if r < 0 or c < 0:
                 continue
-            scores[nr, nc] = max(scores[i, j], scores[nr, nc])
+            scores[r, c] = max(scores[i, j], scores[r, c])
 
     return scores
