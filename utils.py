40be318384e2e663200ccea7157a29dc0369f53e
Alan Davidson <alan@viam.com>
2024-12-23 16:11:53 -0500

    add a way to convert the HSV image to an RGB image early on

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import collections
+import colorsys
 import numpy
 
 # The tokens is a list of tokens contained in a file.
@@ -22,3 +23,30 @@ def to_hsv_matrix(matrix, hues):
         result[:, :, 0] = hues
         result[:, :, 1] = 255  # Saturation
     return result
+
+
+def to_rgb_matrix(matrix, hues):
+    def pixel_hsv_to_rgb(hsv):
+        hsv_floats = hsv.astype(numpy.float64) / 255
+        rgb_floats = colorsys.hsv_to_rgb(*hsv_floats)
+        return numpy.array([x * 255 for x in rgb_floats], numpy.uint8)
+
+    hsv_matrix = to_hsv_matrix(matrix, hues)
+    rgb_matrix = numpy.zeros(hsv_matrix.shape, numpy.uint8)
+    nr, nc, _ = hsv_matrix.shape
+    for r in range(nr):
+        for c in range(nc):
+            rgb_matrix[r, c, :] = pixel_hsv_to_rgb(hsv_matrix[r, c, :])
+
+    return rgb_matrix
+
+matrix = numpy.array([[1, 1], [1, 0]], dtype=numpy.uint8)
+hues = numpy.array([[0, 170], [45, 170]], dtype=numpy.uint32)
+rgb = to_rgb_matrix(matrix, hues)
+bw = to_rgb_matrix(matrix, None)
+
+print(matrix)
+print(hues)
+print(rgb)
+print(bw)
+print(rgb[1, 0, :])
