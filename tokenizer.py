2b7788659ce0f89f735eeefceda31035369492d0
Alan Davidson <alan.davidson@gmail.com>
2024-12-25 11:30:36 -0500

    pull _find_boundary out to a standalone function

--- tokenizer.py
+++ tokenizer.py
@@ -25,48 +25,52 @@ def get_tokens(file_contents, language):
     return FileInfo(token_array, lines, boundaries)
 
 
+def _find_boundary(i, tok, toks, most_recent_line):
+    """
+    This helper function returns the start and end tuples (in (row, col)
+    format) for the given token with the given index into toks.
+    """
+    try:
+        start = tok.ast_node.start_point
+        end = tok.ast_node.end_point
+        most_recent_line = end[0]
+        return start, end
+    except AttributeError:  # Token doesn't have a start_point or end_point
+        if tok.type == "indent":
+            # When we add indentation, it's on the same line as the next
+            # token. Pretend it starts at the beginning of the line and
+            # ends just before the start of the next token.
+            assert(tok.new_line_before)
+            end = toks[i + 1].ast_node.start_point
+            start = (end[0], 0)
+            return start, end
+        elif tok.type == "dedent":
+            # If there are other non-dedent tokens after us, we can use
+            # their starting column as our ending column.
+            next_i = i
+            try:
+                while toks[next_i].type == "dedent":
+                    next_i += 1
+            except IndexError:
+                # No following tokens: we hit EOF. We probably can't display
+                # this token.
+                line = most_recent_line
+                return (line, 0), (line, 0)
+            # Otherwise, we found a non-dedent token: stop where it starts
+            end = toks[next_i].ast_node.start_point
+            return (end[0], 0), end
+        else:
+            print("UNEXPECTED TOKEN!", i, tok, type(tok), dir(tok))
+            raise
+
+
 def _get_boundaries(toks):
     most_recent_line = 0  # Used when parsing dedents in Python
-
-    def find_boundary(i, tok):  # Helper function
-        nonlocal most_recent_line
-        try:
-            start = tok.ast_node.start_point
-            end = tok.ast_node.end_point
-            most_recent_line = end[0]
-            return start, end
-        except AttributeError:  # Token doesn't have a start_point or end_point
-            if tok.type == "indent":
-                # When we add indentation, it's on the same line as the next
-                # token. Pretend it starts at the beginning of the line and
-                # ends just before the start of the next token.
-                assert(tok.new_line_before)
-                end = toks[i+1].ast_node.start_point
-                start = (end[0], 0)
-                return start, end
-            elif tok.type == "dedent":
-                # If there are other non-dedent tokens after us, we can use
-                # their starting column as our ending column.
-                next_i = i
-                try:
-                    while toks[next_i].type == "dedent":
-                        next_i += 1
-                except IndexError:
-                    # No following tokens: we hit EOF. We probably can't display
-                    # this token.
-                    line = most_recent_line
-                    return (line, 0), (line, 0)
-                # Otherwise, we found a non-dedent token: stop where it starts
-                end = toks[next_i].ast_node.start_point
-                return (end[0], 0), end
-            else:
-                print("UNEXPECTED TOKEN!", i, tok, type(tok), dir(tok))
-                raise
-
     boundaries = []
     for i, tok in enumerate(toks):
-        start, end = find_boundary(i, tok)
+        start, end = _find_boundary(i, tok, toks, most_recent_line)
         # The tokenizer we use starts counting lines at 0, and we need to start
         # counting at 1. So, add 1 to all line indices.
         boundaries.append(((start[0] + 1, start[1]), (end[0] + 1, end[1])))
+        most_recent_line = end[0]
     return boundaries
