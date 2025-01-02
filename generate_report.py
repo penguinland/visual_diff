a3e8e59ff6d028d8e97f718e3f3d8c556cfc1db4
Alan Davidson <alan.davidson@gmail.com>
2025-01-01 23:05:45 -0500

    make proof-of-concept report, needs to be refactored

--- generate_report.py
+++ generate_report.py
@@ -4,6 +4,7 @@ import collections
 import glob
 
 import find_duplicates
+import tokenizer
 import utils
 
 
@@ -22,7 +23,7 @@ def find_all_files(glob_pattern):
 
 def compare_files(filename_a, filename_b, language):
     data_a, data_b = (tokenizer.get_file_tokens(filename, language)
-                      for filename in (args.filename_a, args.filename_b))
+                      for filename in (filename_a, filename_b))
     pixel_count = len(data_a.tokens) * len(data_b.tokens)
     if pixel_count > utils.PIXELS_IN_BIG_FILE:
         print("skipping analysis of too-big image "
@@ -30,17 +31,40 @@ def compare_files(filename_a, filename_b, language):
         return
     matrix = utils.make_matrix(data_a.tokens, data_b.tokens)
     segments = find_duplicates.get_segments(matrix, (filename_a == filename_b))
-    # We'll keep a tuple of (size, start_line_a, end_line_a, start_line_b,
-    # end_line_b) for each large segment we find.
+    # We'll keep a tuple of (negative_size, start_line_a, end_line_a,
+    # start_line_b, end_line_b) for each large segment we find. We store the
+    # negative of the size so that, when sorted, the largest segments come
+    # first.
     large_segments = set()
     for segment in segments:
         if segment.size() < 100:
             continue
-        large_segments.add((
+        # When comparing a file to itself, don't consider the segment from X to
+        # Y as distinct from the segment from Y to X.
+        if filename_a == filename_b and segment.top[0] > segment.top[1]:
+                continue
+        large_segments.add((-segment.size(),
+                            data_a.boundaries[segment.top[0]][0][0],
+                            data_a.boundaries[segment.bottom[0]][1][0],
+                            data_b.boundaries[segment.top[1]][0][0],
+                            data_b.boundaries[segment.bottom[1]][1][0],
+                            ))
 
+    if not large_segments:
+        return  # No major duplication!
+    # Otherwise...
+    print(f"Found duplicated code between {filename_a} and {filename_b}:")
+    for size, start_a, end_a, start_b, end_b in sorted(large_segments):
+        print(f"  {-size} tokens on lines "
+              f"{start_a}-{end_a} and {start_b}-{end_b}")
+
+
+compare_files("examples/pointsprite.py", "examples/pointsprite.py", "python")
 
 
 def compare_all_files(filenames, language):
-    for filename_a in filenames:
-        for filename_b in filenames:
+    # Compare all pairs of files. After comparing A with B, don't also compare
+    # B with A, but do remember to compare A with A.
+    for i, filename_a in enumerate(filenames):
+        for filename_b in filenames[i:]:
             compare_files(filename_a, filename_b, language)
