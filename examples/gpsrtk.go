bbb89750e68d8ab0801354fa313784634c8b5e30
Alan Davidson <alan@viam.com>
2025-01-03 16:27:10 -0500

    Prevent flakiness in integration test (#23)
    Surprise! My two computers somehow got different tokenizers for Go. Consider this example:
    ```
    `trouble`
    ```
    One would tokenize that as a single token containing everything from one backtick to the other (including both backticks), and the other would tokenize it as 3 separate tokens: a backtick, a string containing `"trouble"`, and another backtick.
    
    Rather than figure out if this is because tree-sitter updated something, or because one of my machines uses go 1.21 and the other uses go 1.20, or it's OS-dependent, or what, the simple thing to do is to not use backticks in the integration test. :upside_down_face: 
    
    
    * write extra stuff
    
    * redo GPS (go) images without backticks
    
    * Revert "write extra stuff"
    
    This reverts commit 6e6e02f1a5ab889aff8233db53a6404582b24dcb.

--- examples/gpsrtk.go
+++ examples/gpsrtk.go
@@ -30,6 +30,13 @@ import (
 	rdkutils "go.viam.com/rdk/utils"
 )
 
+// As of late 2024, something about the way tree-sitter parsed backticks was not consistent: one
+// computer running this would get a single token containing two backticks and everything between,
+// while another would get three tokens: a backtick token, then the string between them, and
+// another backtick token. In order to get the integration tests to pass with either tokenizer
+// implementation, we comment out all of these structs. The code won't compile, but at least you
+// can find the other duplications.
+/*
 // AttrConfig is used for converting NMEA MovementSensor with RTK capabilities config attributes.
 type AttrConfig struct {
 	CorrectionSource string `json:"correction_source"`
@@ -67,6 +74,7 @@ type I2CAttrConfig struct {
 	I2cAddr     int    `json:"i2c_addr"`
 	I2CBaudRate int    `json:"i2c_baud_rate,omitempty"`
 }
+*/
 
 // Validate ensures all parts of the config are valid.
 func (cfg *AttrConfig) Validate(path string) ([]string, error) {
